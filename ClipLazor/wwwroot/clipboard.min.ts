ce96682aba198f5d1a7b87eda8c47ff17236a291
p6laris <yunissurchy@gmail.com>
2024-11-15 07:38:21 +0300

    Does not need to check for Permissions in non-Chromium browsers as most of them does not support it. Fixes: #3

--- ClipLazor/wwwroot/clipboard.min.ts
+++ ClipLazor/wwwroot/clipboard.min.ts
@@ -1,38 +1,51 @@
-﻿namespace ClipLazor{
-    interface IClipboardFunctionality{
-        isClipboardSupported() : boolean;
-        hasClipboardPermission(command : PermissionName) : Promise<boolean>;
+﻿namespace ClipLazor {
+    interface IClipboardFunctionality {
+        isClipboardSupported(): boolean;
+        hasClipboardPermission(command: PermissionName): Promise<boolean>;
         copyToClipboard(text: string): Promise<boolean>;
         pasteFromClipboard(): Promise<string | null>;
         copyDataToClipboard(data: Uint8Array, mimeType: string): Promise<boolean>;
         readDataFromClipboard(mimeType: string): Promise<Uint8Array | null>;
-        
+
     }
-    
-    class Clipboard implements IClipboardFunctionality{
+
+    class Clipboard implements IClipboardFunctionality {
+
+        private readonly isChromium: boolean;
+
+        constructor() {
+            // Check for Chromium-based browser
+            const userAgentData = (navigator as any).userAgentData;
+            const isChromium = !!userAgentData && userAgentData.brands.some(data => data.brand == 'Chromium');
+        }
         /**
          * Checks if the Clipboard API is supported in the current browser.
          *
          * @returns {boolean} `true` if the Clipboard API is supported, otherwise `false`.
          */
-            isClipboardSupported(): boolean {
+        isClipboardSupported(): boolean {
             return !!navigator.clipboard;
         }
 
         /**
          * Checks if the browser has permission to write to the clipboard.
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API#security_considerations
+         * 
          *
          * @param {PermissionName} command - The permission name to check (e.g., 'clipboard-write').
          * @returns {Promise<boolean>} A Promise that resolves to `true` if the permission is granted, otherwise `false`.
          */
-          async hasClipboardPermission(command: PermissionName): Promise<boolean> {
-            try {
-                const permissionStatus = await navigator.permissions.query({ name: command });
-                return permissionStatus.state === 'granted';
-            } catch (error) {
-                console.error(`Error checking ${command} permission:`, error);
-                return false;
+        async hasClipboardPermission(command: PermissionName): Promise<boolean> {
+            if (this.isChromium) {
+                try {
+                    const permissionStatus = await navigator.permissions.query({ name: command });
+                    return permissionStatus.state === 'granted';
+                } catch (error) {
+                    console.error(`Error checking ${command} permission:`, error);
+                    return false;
+                }
             }
+            return true;
         }
 
         /**
@@ -41,7 +54,7 @@
          * @param {string} text - The text to be copied to the clipboard.
          * @returns {Promise<boolean>} A Promise that resolves to `true` if the copy operation is successful, otherwise `false`.
          */
-          async copyToClipboard(text: string): Promise<boolean> {
+        async copyToClipboard(text: string): Promise<boolean> {
             try {
                 await navigator.clipboard.writeText(text);
                 return true;
@@ -56,7 +69,7 @@
          *
          * @returns {Promise<string | null>} A Promise that resolves to the copied text if successful, otherwise `null`.
          */
-          async pasteFromClipboard(): Promise<string | null> {
+        async pasteFromClipboard(): Promise<string | null> {
             try {
                 const text = await navigator.clipboard.readText();
                 return text;
@@ -73,7 +86,7 @@
          * @param {string} mimeType - The MIME type of the data.
          * @returns {Promise<boolean>} A Promise that resolves to `true` if the copy operation is successful, otherwise `false`.
          */
-          async copyDataToClipboard(data: Uint8Array, mimeType: string): Promise<boolean> {
+        async copyDataToClipboard(data: Uint8Array, mimeType: string): Promise<boolean> {
             try {
                 const blob = new Blob([data], { type: mimeType });
                 await navigator.clipboard.write([new ClipboardItem({ [mimeType]: blob })]);
@@ -90,7 +103,7 @@
          * @param {string} mimeType - The MIME type of the data to read.
          * @returns {Promise<Uint8Array | null>} A Promise that resolves to the binary data if found, otherwise `null`.
          */
-          async readDataFromClipboard(mimeType: string): Promise<Uint8Array | null> {
+        async readDataFromClipboard(mimeType: string): Promise<Uint8Array | null> {
             try {
                 const clipboardItems = await navigator.clipboard.read();
                 for (const item of clipboardItems) {
